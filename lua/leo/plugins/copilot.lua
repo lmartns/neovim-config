fda63277fa491795b968ba29d08453856b1a3239
lmartns <leonardomartins578@gmail.com>
2025-04-27 11:05:09 -0300

    feat: plugins/ copilot and markdown

--- lua/leo/plugins/copilot.lua
+++ lua/leo/plugins/copilot.lua
@@ -6,6 +6,21 @@ return {
       { "nvim-lua/plenary.nvim", branch = "master" },
     },
     build = "make tiktoken",
-    opts = {},
+    opts = {
+      disable_completion = true,
+    },
+    config = function()
+      vim.g.copilot_enabled = 0
+
+      vim.api.nvim_create_user_command("CopilotToggle", function()
+        if vim.g.copilot_enabled == 0 then
+          vim.g.copilot_enabled = 1
+          print("Copilot ativado")
+        else
+          vim.g.copilot_enabled = 0
+          print("Copilot desativado")
+        end
+      end, {})
+    end,
   },
 }
