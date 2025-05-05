f54e269d4b88a90682e18a519734e30ac854e0b9
lmartns <leonardomartins578@gmail.com>
2025-05-05 07:35:33 -0700

    remove copy fucntion for linux machines

--- init.lua
+++ init.lua
@@ -1,19 +1,6 @@
 require("leo.core")
 require("leo.lazy")
 
-vim.g.clipboard = {
-  name = "wl-clipboard",
-  copy = {
-    ["+"] = "wl-copy",
-    ["*"] = "wl-copy",
-  },
-  paste = {
-    ["+"] = "wl-paste --no-newline",
-    ["*"] = "wl-paste --no-newline",
-  },
-  cache_enabled = 1,
-}
-
 -- No seu init.lua ou arquivo de configuração
 vim.diagnostic.config({
   virtual_text = true,   -- Desativa texto virtual no final da linha
