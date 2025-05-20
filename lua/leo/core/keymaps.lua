9ffb24f8b1aafd47b0f27ed1ebb6599790805347
lmartns <leonardomartins578@gmail.com>
2025-05-20 08:24:50 -0300

    update

--- lua/leo/core/keymaps.lua
+++ lua/leo/core/keymaps.lua
@@ -12,13 +12,13 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- incremen
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })     -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })   -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })      -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line(s) down" }) -- move selected line(s) down
-keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line(s) up" }) -- move selected line(s) up
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line(s) up" })   -- move selected line(s) up
 
 keymap.set("n", "<C-d>", "<C-d>zz")
 keymap.set("n", "<C-u>", "<C-u>zz")
@@ -42,9 +42,5 @@ keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>", { desc = "O
 keymap.set("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<CR>", { desc = "Open horizontal terminal" })
 
 keymap.set("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical<CR>", { desc = "Open vertical terminal" })
-
-keymap.set("n", "<space>fb", function()
-  require("telescope").extensions.file_browser.file_browser()
-end)
 
 vim.keymap.set("n", "<Tab>", "i<Tab><Esc>l", { noremap = true })
