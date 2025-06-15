8d1e097f9d0e5800619773f6ce429063e174caf1
lmartns <leonardomartins578@gmail.com>
2025-06-15 13:25:12 -0300

    update

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,4 +1,3 @@
--- set leader key to space
 vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
@@ -7,18 +6,16 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
--- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })     -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })   -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })      -- make split windows equal width & height
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line(s) down" }) -- move selected line(s) down
-keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line(s) up" })   -- move selected line(s) up
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line(s) up" }) -- move selected line(s) up
 
 keymap.set("n", "<C-d>", "<C-d>zz")
 keymap.set("n", "<C-u>", "<C-u>zz")
