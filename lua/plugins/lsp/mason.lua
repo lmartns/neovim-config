ddfb2bac63710972ac3f15c7887e8dffca5f1a3c
lmartns <leonardomartins578@gmail.com>
2025-06-20 03:09:42 -0300

    fix mason

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -3,27 +3,30 @@ return {
   dependencies = {
     "mason-org/mason-lspconfig.nvim",
   },
+
   ensure_installed = {
     "stylua",
     "prettier",
+    "astro",
+    "biome",
+    "lua_ls",
+    "ts_ls",
+    "html",
+    "cssls",
+    "sqlls",
+    "clangd",
+    "gopls",
+    "rust-analyzer",
+    "svelteserver",
   },
+
   config = function()
     local mason = require("mason")
     local mason_lspconfig = require("mason-lspconfig")
 
-    mason.setup({ PATH = "append" })
+    mason.setup()
 
     mason_lspconfig.setup({
-      ensure_installed = {
-        "astro",
-        "biome",
-        "lua_ls",
-        "ts_ls",
-        "html",
-        "cssls",
-        "astro",
-        "biome",
-      },
       automatic_installation = true,
     })
   end,
